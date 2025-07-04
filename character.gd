f93b47f70190068e45205da18443a95811eca202
dginovker <danielginovker@gmail.com>
2025-07-04 10:51:10 -0700

    Get power and collisions working better

--- character.gd
+++ character.gd
@@ -1,18 +1,28 @@
 extends CharacterBody2D
 class_name Character
 
-@export var max_power := 50
+@export var max_power := 2000
 
 var _input_pressed := false
 var _power = 0
 
 func _input(event: InputEvent) -> void:
     _input_pressed = event.is_pressed()
-    
-func _process(delta: float) -> void:
+        
+func _physics_process(delta: float) -> void:
     if _input_pressed:
-        _power += delta
+        _power += min(1000 * delta, max_power)
     else:
+        if is_on_floor():
+            velocity.y -= _power
+            velocity.x += _power 
         _power = 0
-    Connector.hud.update_power(min(_power*20, max_power))
+    Connector.hud.update_power(_power)
+    Connector.hud.set_debug(["Velocity: ", Vector2i(velocity)])
+
+    velocity.y += delta * 200 # Gravity go brr
+    move_and_slide()
+    
+    if is_on_floor():
+        velocity = Vector2.ZERO            
     
--- hud.gd
+++ hud.gd
@@ -5,4 +5,7 @@ func _ready() -> void:
     Connector.register_hud(self)
 
 func update_power(power: float):
-    $PanelContainer/Label.text = "Power: " + str(int(power))
+    $PanelContainer/VBoxContainer/PowerLabel.text = "Power: " + str(int(power))
+    
+func set_debug(args: Array):
+    $PanelContainer/VBoxContainer/DebugLabel.text = str(args)
