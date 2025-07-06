8d4c6bb2796fe99e3c3696762338e60fc7416a0d
dginovker <danielginovker@gmail.com>
2025-07-05 22:22:43 -0700

    Ok another checkpoint because I need to roll back to 4.5.2 since I actually have export tempaltes for that so I can export a server and so I can and and and

--- GameManager.gd
+++ GameManager.gd
@@ -2,7 +2,7 @@ extends Node
 
 func restart(player: Character) -> void:
     #player.position = Vector2(0, -100)
-    player.position = Vector2(5400, -1200)
+    player.position = Vector2(7100, -1200)
     player.linear_velocity = Vector2.ZERO
     for obj: Node in player.touched_objs:
         obj.visible = true
