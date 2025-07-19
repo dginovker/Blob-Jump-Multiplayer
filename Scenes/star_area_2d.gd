10c3622bb0041c79d53296d0906ce774a215b2ab
dginovker <danielginovker@gmail.com>
2025-07-19 15:41:23 -0700

    Change moving block to do client reconciliation for more smoothness

--- Scenes/star_area_2d.gd
+++ Scenes/star_area_2d.gd
@@ -4,7 +4,9 @@ func _ready() -> void:
     body_entered.connect(_on_body_entered)
     
 func _on_body_entered(body: Node) -> void:
-    if body is Character:
-        visible = false
-        (body as Character).touched_stars[self] = true
-    
+    if body is not Character:
+        return
+    if not (body as Character).is_multiplayer_authority():
+        return        
+    visible = false
+    (body as Character).touched_stars[self] = true
--- moving_platform.gd
+++ moving_platform.gd
@@ -3,13 +3,33 @@ extends AnimatableBody2D
 @export var move_speed := 250
 @onready var path_follow: PathFollow2D = get_parent()
 
-var path_direction = 1
+var _path_direction := 1
+var _progress_tween: Tween
+var _sync_timer := 0.2
 
 func _physics_process(delta: float) -> void:
-    if not multiplayer.is_server():
-        return
-    path_follow.progress += move_speed * delta * path_direction
+    path_follow.progress += move_speed * delta * _path_direction
     if path_follow.progress_ratio <= 0\
         or path_follow.progress_ratio >= 1:
-        path_direction *= -1
-        
+        _path_direction *= -1
+
+    if not multiplayer.is_server():
+        return
+    
+    _sync_timer -= delta
+    if _sync_timer < 0:
+        _sync_timer = 0.2
+        reconcile.rpc(path_follow.progress, _path_direction)
+
+@rpc("authority", "call_local")
+func reconcile(server_progress: float, server_direction: int):
+    if multiplayer.is_server():
+        return
+    _path_direction = server_direction
+
+    if _progress_tween:
+        _progress_tween.kill()
+    _progress_tween = create_tween()
+    _progress_tween.tween_property(
+        path_follow, "progress", server_progress, 1.0
+    ).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
